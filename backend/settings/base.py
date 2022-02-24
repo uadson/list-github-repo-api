6a7f384ec73301cb542d182349ec802831fbdc8a
uadson <uadsoncastro@gmail.com>
2022-02-24 13:03:43 -0300

    criação do app core

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local Apps
-
+    'core',
 
     # 3rd Apps
     'rest_framework',
---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core.views import HomeView
+
+
+app_name = 'core'
+
+urlpatterns = [
+	path('', HomeView.as_view(), name=home),
+]
