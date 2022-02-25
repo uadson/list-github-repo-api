d0664b188f376e994c10a8ae8728bf6755390aac
uadson <uadsoncastro@gmail.com>
2022-02-25 13:02:15 -0300

    estruturação dos models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+
+	class Meta:
+		abstract = True
+
+
+class Repository(Base):
+	name = models.CharField('Name:', max_length=200)
+	url = models.URLField('URL:', max_length=200)
+	
