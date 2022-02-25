443dfdd8405e8e692e432bafca4169bc5e4b981f
uadson <uadsoncastro@gmail.com>
2022-02-25 14:27:26 -0300

    adicionado aplicação app

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    # ADMIN
     path('admin/', admin.site.urls),
+
+    # APP
+    path('', include('app.urls', namespace='app')),
 ]
