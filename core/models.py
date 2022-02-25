b5476d3572f20b8e5f02ce90946877e5f4796ddf
uadson <uadsoncastro@gmail.com>
2022-02-25 14:28:34 -0300

    incluidas configurações para customização de usuário.

--- core/models.py
+++ core/models.py
@@ -1,16 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Base(models.Model):
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	active = models.BooleanField(default=True)
-
-	class Meta:
-		abstract = True
-
-
-class Repository(Base):
-	name = models.CharField('Name:', max_length=200)
-	url = models.URLField('URL:', max_length=200)
-	
+class User(AbstractUser):
+    pass
