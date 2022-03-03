96c5499570719f4c7457ff047202c40485406596
uadson <uadsoncastro@gmail.com>
2022-03-03 14:29:03 -0300

    arquivo para teste das funções

---
+++ utils/test_get_api_commit.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import os
+
+
+class GetApi:
+    def __init__(self, user):
+        self._user = user
+        self.main_dir = os.getcwd()
+        self.api_dir = os.path.join(self.main_dir, 'api')
+        self.lang_dir = os.path.join(self.main_dir, 'lang')
+        self.repos_dir = os.path.join(self.main_dir, 'repos')
+
+    def get_api_repos(self):
+        url = requests.get(
+            f'https://api.github.com/users/{self._user}/repos'
+        )
+        try:
+            response = url.text
+            
+            with open(os.path.join(self.api_dir, 'data.txt'), 'w') as file:
+                for line in response:
+                    file.write(str(line))
+
+        except:
+            return url.status_code
+
+    def get_api_commits(self):
+        user = 'uadson'
+        with open(os.path.join(self.api_dir, 'data.txt'), 'r') as file:
+            data = file.read()
+            repos = json.loads(data)
+            for i in range(len(repos)):
+                url = requests.get(f'https://api.github.com/repos/{user}/{repos[i]["name"]}/commits')
+                # response = url.text
+                response = url.json()
+              
+                with open(os.path.join(self.repos_dir, f"{repos[i]['name']}.txt"), 'w', encoding='cp1252') as file:
+                    for line in response:
+                        file.write(str(line))
+
+
+if __name__ == '__main__':
+
+    data = GetApi('uadson')
+    # data.get_api_repos()
+    data.get_api_commits()
+    # data.get_api_languages()
+    # data.get_api_data()
