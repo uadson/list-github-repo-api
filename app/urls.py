4ce10c3dfc35b90bd47f08d117c3ebaf1af4c1c3
Uadson Feitosa <uadsoncastro@gmail.com>
2022-03-02 22:43:03 -0300

    criacao de tabelas e rotas

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from app.views.api import ListRepoApiView
 
-from app.views import HomeView
+from app.views.home import HomeView
 
 
 app_name = 'app'
 
 urlpatterns = [
+	# Home
 	path('', HomeView.as_view(), name='home'),
+
+	# API
+	path('api/repos/', ListRepoApiView.as_view(), name='api_repos'),
 ]
