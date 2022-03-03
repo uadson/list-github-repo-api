14ca72393e48ee302f38a63f8c6ed24a4a6b04a2
uadson <uadsoncastro@gmail.com>
2022-03-03 14:21:04 -0300

    alteracao em models para inclusao da api no campo jsonfield e display em admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
 
+from app.models import Repository
+
+
+@admin.register(Repository)
+class RepoAdmin(admin.ModelAdmin):
+	pass
--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+from decouple import config
+
+from utils.get_api_data import GetApi
+
+
+class Repository(models.Model):
+	user = models.CharField(max_length=20)
+	data = models.JSONField(blank=True)
+
+	class Meta:
+		verbose_name = 'respository'
+		verbose_name_plural = 'repositories'
+		db_table = 'repositories'
+
+	def save(self, *args, **kwargs):
+		if self.data is None:
+			data = GetApi(self.user)
+			self.data = data.get_api_data()
+		super().save(*args, **kwargs)
